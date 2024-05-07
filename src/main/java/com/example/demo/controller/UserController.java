8b13825c1d09041f53c7e5c8295543bf185ffdef
Ramindu Jayasinghe <2021cs086@stu.ucsc.cmb.ac.lk>
2024-05-07 17:55:46 +0530

    saveUsers

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @GetMapping("/getUser")
+    public String getUser() {
+        return "Simple User";
+    }
+
+
+    @PostMapping("/saveUser")
+    public String saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser() {
+        return "User Updated";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser() {
+        return "User Deleted";
+    }
+
+}
---
+++ src/main/java/com/example/demo/dto/userDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class userDTO {
+    private int id;
+    private String name;
+    private String address;
+}
